1a34b20a4ab369291032902becf068d74fcaa945
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2023-01-18 20:09:26 +0100

    Adds proper credits in one file

--- Gim2png_bat/Gim2png.bat
+++ Gim2png_bat/Gim2png.bat
@@ -1,4 +1,7 @@
 :: This script automates the process 
 :: of converting the GIM files to PNG
 
+:: Code adapted (copied into a bat file) from akadewboy 
+:: https://forum.xentax.com/viewtopic.php?t=6313
+
 start "%~dp0" gimconv "test.gim" -o "out_test.png"
