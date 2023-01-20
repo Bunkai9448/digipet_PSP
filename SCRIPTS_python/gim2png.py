85dcc64f3d14b8e8f84523483c3b3512123545b4
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2023-01-20 08:36:07 +0100

    Adds python script to convert gim to png
    This is for a batch of files instead just the POC from the bat file

---
+++ SCRIPTS_python/gim2png.py
@@ -0,0 +1,19 @@
+# Python script to convert GIM files to PNG
+
+import os
+import sys
+
+# folder path
+dir_path = "./"
+
+# creates folder to save new files
+os.system("mkdir PNG \n")
+
+# loop through files
+for i in os.listdir(dir_path):
+	if i.endswith('.GIM') :
+		input = str(i)
+		output = input + ".png"
+		os.system("gimconv " + input + " -o " + "./PNG/" + output + "\n")
+
+print("files converted")
