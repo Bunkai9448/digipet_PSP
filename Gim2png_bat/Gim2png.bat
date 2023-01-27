bb55ced5940ce772ef0449c951526bef03a1108c
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2023-01-27 22:34:11 +0100

    Adds full list of tasks to the sample

--- Gim2png_bat/Gim2png.bat
+++ Gim2png_bat/Gim2png.bat
@@ -1,7 +1,16 @@
-:: This script automates the process 
-:: of converting the GIM files to PNG
+:: Based on the post by akadewboy on Fri Apr 01, 2011 9:11 pm
+:: in https://forum.xentax.com/viewtopic.php?t=6313
+:: With snipped code to add -qbb option in GimConv.cfg file, by "mono21400" also known as "ethanol"
 
-:: Code adapted (copied into a bat file) from akadewboy 
-:: https://forum.xentax.com/viewtopic.php?t=6313
+::1. Convert it to PNG using GimConv
 
-start "%~dp0" gimconv "test.gim" -o "out_test.png"
+gimconv "GIM_000000bd.GIM" -o "GIM_000000bd.PNG" --index 4 -qbb
+pause
+:: 2. 3. & 4. Edit s_ui_mp_01.png with whatever graphic program you want.
+:: 	Work with PNG in 32bit PNG
+::  Save it in 8bit+Alpha PNG
+
+:: 5. Use GimConv to convert s_ui_mp_01(32bit)-nq8.png into a GIM
+
+gimconv "GIM_000000bd.PNG" -o "GIM_000000bd1.GIM" --index 4 -qbb
+pause
