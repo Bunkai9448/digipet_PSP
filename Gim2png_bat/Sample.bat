1d233af056c1cb22b7e06d231f8d7c52bd2345a8
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2023-01-28 02:42:44 +0100

    Rename sample file and fix commands

--- Gim2png_bat/Sample.bat
+++ Gim2png_bat/Sample.bat
@@ -2,9 +2,12 @@
 :: in https://forum.xentax.com/viewtopic.php?t=6313
 :: With snipped code to add -qbb option in GimConv.cfg file, by "mono21400" also known as "ethanol"
 
+:: "input.GIM" == "output.GIM" sanity check:
+:: gimconv "input.GIM" -o "output.GIM" -digi 
+
 ::1. Convert it to PNG using GimConv
 
-gimconv "GIM_000000bd.GIM" -o "GIM_000000bd.PNG" --index 4 -qbb
+gimconv "GIM_000000bd.GIM" -o "GIM_000000bd.PNG" -digi
 pause
 :: 2. 3. & 4. Edit s_ui_mp_01.png with whatever graphic program you want.
 :: 	Work with PNG in 32bit PNG
@@ -12,5 +15,5 @@ pause
 
 :: 5. Use GimConv to convert GIM_000000bd.PNG into a GIM
 
-gimconv "GIM_000000bd.PNG" -o "GIM_000000bd1.GIM" --index 4 -qbb
+gimconv "GIM_000000bd.PNG" -o "GIM_000000bd1.GIM" -digi
 pause
